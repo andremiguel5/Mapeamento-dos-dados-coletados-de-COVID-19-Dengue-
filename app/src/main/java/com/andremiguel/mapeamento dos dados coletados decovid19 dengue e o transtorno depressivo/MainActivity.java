9873b9ff78f463b7985a99d0560dd15f61f18d09
andre <andremiguel5@hotmail.com>
2022-11-29 20:03:24 -0300

    start commit

---
+++ app/src/main/java/com/andremiguel/mapeamento dos dados coletados decovid19 dengue e o transtorno depressivo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andremiguel.mapeamentodosdadoscoletadosdecovid19dengueeotranstornodepressivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
